7ef3ae73d077098a05ecc23d7457b854ca64bc8d
Korbinian Stein <kstein@inforbi.de>
2021-06-09 15:01:31 +0200

    Restructure

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import {Server} from "socket.io";
+
+
+const io = new Server(3000, {
+  serveClient: false,
+  // below are engine.IO options
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false,
+  cors: {
+    origin: "*"
+  }
+});
+
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  socket.username = username;
+  if (!socket.username) {
+    return next(new Error("invalid username"));
+  }
+  next();
+})
+
+io.on("connection", (socket => {
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      username: socket.username
+    });
+  }
+  socket.emit("users", users);
+}));
+
+io.on("username:change", (socket => {
+  console.log(socket);
+
+}))
